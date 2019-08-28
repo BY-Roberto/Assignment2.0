edfed88b058d5c01732674061142c4e7838aa0c4
BoyangYu <boyangyu@boyangyu.cn>
2019-08-28 17:14:13 -0500

    findMax&Min

---
+++ src/main/java/findMaxMin.java
@@ -0,0 +1,26 @@
+public class findMaxMin {
+    public Integer findMax(int[] intArray) {
+        if (intArray == null || 0 == intArray.length) {
+            return null;
+        }
+        int max = intArray[0];
+        for (int i = 0; i < intArray.length - 1; i++) {
+            if (max < intArray[i]) {
+                max = intArray[i];
+            }
+        }
+        return max;
+    }
+    public Integer findMin(int[] intArray) {
+        if (intArray == null || 0 == intArray.length) {
+            return null;
+        }
+        int min = intArray[0];
+        for (int i = 0; i < intArray.length - 1; i++) {
+            if (min < intArray[i]) {
+                min = intArray[i];
+            }
+        }
+        return min;
+    }
+}
